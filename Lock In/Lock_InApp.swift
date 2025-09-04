eb670adac68a5592771f54a57af84d4f084db11f
Ayane Yokoya <ayokoya3@gatech.edu>
2025-09-04 14:52:39 -0400

    Initial Commit

---
+++ Lock In/Lock_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lock_InApp.swift
+//  Lock In
+//
+//  Created by Ayane Yokoya on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Lock_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
